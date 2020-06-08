d1af4db9f8e3793b8527ab70a61539089ee275c2
luciaoSainz <danielsainz28@gmail.com>
2020-06-08 23:27:03 +0200

    added config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const path = require('path');
 
+//Initializations
 const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, 'views'));
+
+
+//Middlewares
+app.use(express.urlencoded({extended:false}));
+
+
+//Globals Variables
+
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('hello'); 
+});
+
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 module.exports = app;
