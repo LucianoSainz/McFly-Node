982549fb94d94c3edaad45c5d7d65dd5cbe5f61b
luciaoSainz <danielsainz28@gmail.com>
2020-06-10 01:56:11 +0200

    added Models, Schema, Nav, Bootstrap

--- src/models/User.js
+++ src/models/User.js
@@ -1,22 +1,22 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 
- const UserSchema = new Schema({
-    name: {type:String, required: true},
-    email: {type:String, required: true},
-    password: {type:String, required: true},
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 }, {
     timestamps: true
 });
 
 UserSchema.methods.encrypPassword = async password => {
-  const salt = await bcrypt.genSalt(15);
-  return await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(15);
+    return await bcrypt.hash(password, salt);
 };
 
-UserSchema.methods.matchPassword = function(password){
-   await bcrypt.compare(password, this.password)
+UserSchema.methods.matchPassword = function (password) {
+    return await bcrypt.compare(password, this.password)
 }
 
 module.exports = model('User', UserSchema);
