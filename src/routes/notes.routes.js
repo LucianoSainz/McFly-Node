f7ffb56aee65ff4d3485fd0812c4b9e02edc9fba
luciaoSainz <danielsainz28@gmail.com>
2020-06-10 22:16:48 +0200

    added Routes of Nodejs and Express for Notes (CRUD)

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,31 @@
+const { Router } = require('express')
+const router = Router()
+
+const {
+    renderNoteForm,
+    createNewNote,
+    renderNotes,
+    renderEditForm,
+    updateNote,
+    deleteNote
+} = require('../controllers/notes.controllers');
+
+//NEW NOTE
+router.get('/notes/add', renderNoteForm);
+
+router.post('/notes/add', createNewNote);
+
+//GET ALL NOTE
+router.get('/notes', renderNotes);
+
+//EDIT NOTES
+router.get('/notes/edit/:id', renderEditForm);
+
+router.put('/notes/edit/:id', updateNote);
+
+//DELETE NOTES
+router.delete('/notes/delete/:id', deleteNote);
+
+
+
+module.exports = router
